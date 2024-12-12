ced227c982b10142d172ee73b46cd0f0e345b156
sungmin <trevorme91@gmail.com>
2024-12-12 10:53:12 +0900

    physics wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
 import * as THREE from "three";
+import CANNON from "cannon";
 import GUI from "lil-gui";
 import "./style.css";
 
 function Page() {
   const el = useRef<HTMLCanvasElement>(null);
+
+  /**
+   * Physics
+   */
+  const world = new CANNON.World();
+
+  world.gravity.set(0, -9.82, 0);
 
   useEffect(() => {
     if (!el.current) {
@@ -17,15 +27,98 @@ function Page() {
 
     el.current.innerHTML = "";
     // Scene
-    const scene = new THREE.Scene();
 
     const gui = new GUI();
 
-    const parameters = {
-      materialColor: "#6ab0d2",
-    };
+    /**
+     * Sizes
+     */
 
-    gui.addColor(parameters, "materialColor");
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    /**
+     * Camera
+     */
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    /**
+     * Textures
+     */
+    const textureLoader = new THREE.TextureLoader();
+    const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+    const environmentMapTexture = cubeTextureLoader.load([
+      "/textures/environmentMaps/0/px.png",
+      "/textures/environmentMaps/0/nx.png",
+      "/textures/environmentMaps/0/py.png",
+      "/textures/environmentMaps/0/ny.png",
+      "/textures/environmentMaps/0/pz.png",
+      "/textures/environmentMaps/0/nz.png",
+    ]);
+
+    /**
+     * Test sphere
+     */
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(0.5, 32, 32),
+      new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5,
+      })
+    );
+    sphere.castShadow = true;
+    sphere.position.y = 0.5;
+    scene.add(sphere);
+
+    /**
+     * Floor
+     */
+    const floor = new THREE.Mesh(
+      new THREE.PlaneGeometry(10, 10),
+      new THREE.MeshStandardMaterial({
+        color: "#777777",
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5,
+      })
+    );
+    floor.receiveShadow = true;
+    floor.rotation.x = -Math.PI * 0.5;
+    scene.add(floor);
+
+    /**
+     * Lights
+     */
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2.1);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.set(1024, 1024);
+    directionalLight.shadow.camera.far = 15;
+    directionalLight.shadow.camera.left = -7;
+    directionalLight.shadow.camera.top = 7;
+    directionalLight.shadow.camera.right = 7;
+    directionalLight.shadow.camera.bottom = -7;
+    directionalLight.position.set(5, 5, 5);
+    scene.add(directionalLight);
 
     /**
      * Sizes
@@ -49,22 +142,26 @@ function Page() {
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     });
 
+    // add axis helper
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
+
     /**
      * Camera
      */
-
-    const cameraGroup = new THREE.Group();
-    scene.add(cameraGroup);
-
     // Base camera
     const camera = new THREE.PerspectiveCamera(
-      35,
+      75,
       sizes.width / sizes.height,
       0.1,
-      100
+      1000
     );
-    camera.position.z = 6;
-    cameraGroup.add(camera);
+    camera.position.set(-3, 3, 3);
+    scene.add(camera);
+
+    // Controls
+    const controls = new OrbitControls(camera, el.current);
+    controls.enableDamping = true;
 
     /**
      * Renderer
@@ -76,117 +173,52 @@ function Page() {
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    // Texture
-    const textureLoader = new THREE.TextureLoader();
-    const gradientTexture = textureLoader.load("textures/gradients/3.jpg");
-    gradientTexture.magFilter = THREE.NearestFilter;
-
     /**
      * Objects
      */
-    // Material
-    const material = new THREE.MeshToonMaterial({
-      color: parameters.materialColor,
-      gradientMap: gradientTexture,
+
+    const sphereShape = new CANNON.Sphere(0.5);
+
+    const concreteMaterial = new CANNON.Material("concrete");
+    const plasticMaterial = new CANNON.Material("plastic");
+    const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+      concreteMaterial,
+      plasticMaterial,
+      {
+        friction: 0.3,
+        restitution: 0.5,
+      }
+    );
+    world.addContactMaterial(concretePlasticContactMaterial);
+    const sphereBody = new CANNON.Body({
+      mass: 0.5,
+      position: new CANNON.Vec3(0, 0, 0),
+      material: plasticMaterial,
+      shape: sphereShape,
     });
 
-    // Meshes
-    const mesh1 = new THREE.Mesh(
-      new THREE.TorusGeometry(1, 0.4, 16, 60),
-      material
+    world.addBody(sphereBody);
+
+    const floorShape = new CANNON.Plane();
+    const floorBody = new CANNON.Body();
+    floorBody.material = concreteMaterial;
+    floorBody.mass = 0;
+    floorBody.addShape(floorShape);
+    world.addBody(floorBody);
+    floorBody.quaternion.setFromAxisAngle(
+      new CANNON.Vec3(-1, 0, 0),
+      Math.PI / 2
     );
-    const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
-    const mesh3 = new THREE.Mesh(
-      new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-      material
-    );
-
-    scene.add(mesh1, mesh2, mesh3);
-
-    const objectsDistance = 4;
-
-    mesh1.position.x = 2;
-    mesh2.position.x = -2;
-    mesh3.position.x = 2;
-
-    mesh1.position.y = -objectsDistance * 0;
-    mesh2.position.y = -objectsDistance * 1;
-    mesh3.position.y = -objectsDistance * 2;
-
-    /**
-     * Particles
-     */
-
-    const sectionMeshes = [mesh1, mesh2, mesh3];
-    // Geometry
-    const particlesCount = 1000;
-    const positions = new Float32Array(particlesCount * 3);
-
-    for (let i = 0; i < particlesCount; i++) {
-      positions[i * 3 + 0] = (Math.random() - 0.5) * 6;
-      positions[i * 3 + 1] =
-        objectsDistance * 0.5 -
-        Math.random() * (objectsDistance * sectionMeshes.length);
-      positions[i * 3 + 2] = Math.random() - 0.5;
-    }
-
-    const particlesGeometry = new THREE.BufferGeometry();
-    particlesGeometry.setAttribute(
-      "position",
-      new THREE.BufferAttribute(positions, 3)
-    );
-
-    // Material
-    const particlesMaterial = new THREE.PointsMaterial({
-      color: parameters.materialColor,
-      sizeAttenuation: true,
-      size: 0.03,
-    });
-
-    // Points
-    const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-    scene.add(particles);
 
     /**
      * Lights
      */
-    const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+    // const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
     directionalLight.position.set(1, 1, 0);
     scene.add(directionalLight);
 
-    gui.addColor(parameters, "materialColor").onChange(() => {
-      material.color.set(parameters.materialColor);
-    });
-
     gui.add(directionalLight.position, "x").min(-5).max(5).step(0.01);
 
-    /**
-     * Mousemove Event
-     */
-    const cursor = { x: 0, y: 0 };
-
-    const onMouseMove = (event: any) => {
-      cursor.x = event.clientX / sizes.width - 0.5;
-      cursor.y = event.clientY / sizes.height - 0.5;
-
-      // Normalize cursor position to range [-1, 1]
-      // cursor.x = (event.clientX / window.innerWidth) * 2 - 1;
-      // cursor.y = -(event.clientY / window.innerHeight) * 2 + 1;
-      // Update light position
-      // directionalLight.position.x = cursor.x * 2; // Adjust multiplier as needed
-
-      // directionalLight.position.y = cursor.y * 2; // Adjust multiplier as needed
-    };
-
-    window.addEventListener("mousemove", onMouseMove);
-
-    let scrollY = window.scrollY;
-    window.addEventListener("scroll", () => {
-      scrollY = window.scrollY;
-    });
-    /**
-     * Animate
-     */
     const clock = new THREE.Clock();
     let previousTime = 0;
     let requestId = 0;
@@ -195,31 +227,10 @@ function Page() {
       const elapsedTime = clock.getElapsedTime();
       const deltaTime = elapsedTime - previousTime;
       previousTime = elapsedTime;
-
-      // Animate camera
-      camera.position.y = (-scrollY / sizes.height) * objectsDistance;
-
-      const parallaxX = cursor.x * 0.5;
-      const parallaxY = -cursor.y * 0.5;
-
-      cameraGroup.position.x +=
-        (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
-      cameraGroup.position.y +=
-        (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
-
-      // Animate meshes
-      for (const mesh of sectionMeshes) {
-        mesh.rotation.x = deltaTime * 0.1;
-        mesh.rotation.y = deltaTime * 0.12;
-      }
-
       // Render
+      world.step(1 / 60, deltaTime, 3);
+      sphere.position.copy(sphereBody.position);
       renderer.render(scene, camera);
-
-      // constantly change the position of the light to make it seem like the object is glowing
-      // directionalLight.position.x = Math.sin(elapsedTime);
-      // directionalLight.position.y = Math.cos(elapsedTime);
-
       // Call tick again on the next frame
       requestId = window.requestAnimationFrame(tick);
     };
@@ -228,23 +239,11 @@ function Page() {
 
     return () => {
       cancelAnimationFrame(requestId);
-      window.removeEventListener("mousemove", onMouseMove);
     };
   });
 
   return (
-    <>
-      <canvas className="webgl" style={{ display: "block" }} ref={el}></canvas>
-      <section className="section">
-        <h1>My Portfolio</h1>
-      </section>
-      <section className="section">
-        <h2>My projects</h2>
-      </section>
-      <section className="section">
-        <h2>Contact me</h2>
-      </section>
-    </>
+    <canvas className="webgl" style={{ display: "block" }} ref={el}></canvas>
   );
 }
 
