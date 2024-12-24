48726eb7f699a1c8d983b3d557d39a37f7c9cdc7
sungmin <trevorme91@gmail.com>
2024-12-24 15:50:37 +0900

    revert to default setup(3 spheres)

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { useEffect, useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import * as THREE from "three";
-import CANNON from "cannon";
 import GUI from "lil-gui";
 import "./style.css";
 
@@ -16,9 +15,6 @@ function Page() {
   /**
    * Physics
    */
-  const world = new CANNON.World();
-
-  world.gravity.set(0, -9.82, 0);
 
   useEffect(() => {
     if (!el.current) {
@@ -71,7 +67,11 @@ function Page() {
     ]);
 
     /**
-     * Test sphere
+     * Objects
+     */
+
+    /**
+     * 3 Test sphere pieces
      */
     const sphere = new THREE.Mesh(
       new THREE.SphereGeometry(0.5, 32, 32),
@@ -84,7 +84,34 @@ function Page() {
     );
     sphere.castShadow = true;
     sphere.position.y = 0.5;
+    sphere.position.x = 0.5;
     scene.add(sphere);
+
+    const sphere2 = new THREE.Mesh(
+      new THREE.SphereGeometry(0.5, 32, 32),
+      new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5,
+      })
+    );
+
+    const sphere3 = new THREE.Mesh(
+      new THREE.SphereGeometry(0.5, 32, 32),
+      new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5,
+      })
+    );
+    sphere2.position.y = 1.5;
+    sphere2.position.x = 1.5;
+    scene.add(sphere2);
+    sphere3.position.y = 2.5;
+    sphere3.position.x = 2.5;
+    scene.add(sphere3);
 
     /**
      * Floor
@@ -101,7 +128,7 @@ function Page() {
     );
     floor.receiveShadow = true;
     floor.rotation.x = -Math.PI * 0.5;
-    scene.add(floor);
+    // scene.add(floor);
 
     /**
      * Lights
@@ -144,7 +171,7 @@ function Page() {
 
     // add axis helper
     const axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
+    // scene.add(axesHelper);
 
     /**
      * Camera
@@ -174,43 +201,6 @@ function Page() {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     /**
-     * Objects
-     */
-
-    const sphereShape = new CANNON.Sphere(0.5);
-
-    const concreteMaterial = new CANNON.Material("concrete");
-    const plasticMaterial = new CANNON.Material("plastic");
-    const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-      concreteMaterial,
-      plasticMaterial,
-      {
-        friction: 0.3,
-        restitution: 0.5,
-      }
-    );
-    world.addContactMaterial(concretePlasticContactMaterial);
-    const sphereBody = new CANNON.Body({
-      mass: 0.5,
-      position: new CANNON.Vec3(0, 0, 0),
-      material: plasticMaterial,
-      shape: sphereShape,
-    });
-
-    world.addBody(sphereBody);
-
-    const floorShape = new CANNON.Plane();
-    const floorBody = new CANNON.Body();
-    floorBody.material = concreteMaterial;
-    floorBody.mass = 0;
-    floorBody.addShape(floorShape);
-    world.addBody(floorBody);
-    floorBody.quaternion.setFromAxisAngle(
-      new CANNON.Vec3(-1, 0, 0),
-      Math.PI / 2
-    );
-
-    /**
      * Lights
      */
     // const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
@@ -228,8 +218,6 @@ function Page() {
       const deltaTime = elapsedTime - previousTime;
       previousTime = elapsedTime;
       // Render
-      world.step(1 / 60, deltaTime, 3);
-      sphere.position.copy(sphereBody.position);
       renderer.render(scene, camera);
       // Call tick again on the next frame
       requestId = window.requestAnimationFrame(tick);
